ef7ccfb4b64a703740cf446b4e502236f1608e8a
Nitish Gundherva <nitgundherva@gmail.com>
2014-02-15 14:29:36 +0530

    Version 1.0.5

--- css/mod_nteam.php
+++ css/mod_nteam.php
@@ -10,6 +10,9 @@ $color = $_SESSION['color'];
 $width = $_SESSION['width'];
 $border_width = $_SESSION['border_width'];
 $height = $_SESSION['height'];
+$titleFontSize= $_SESSION['titleFontSize'];
+$field1FontSize= $_SESSION['field1FontSize'];
+$field2FontSize= $_SESSION['field2FontSize'];
 ?>
 <style type="text/css">
 #nteam
@@ -20,33 +23,59 @@ $height = $_SESSION['height'];
 {
 	width: auto;
 }
+#nteam a
+{
+	text-decoration:none;
+}
+#nteam .title
+{
+	font-size: <?php echo $titleFontSize;?>px !important;
+	text-align: center;
+}
+#nteam .field1
+{
+	font-size: <?php echo $field1FontSize;?>px !important;
+	text-align: center;
+}
+#nteam .field2
+{
+	font-size: <?php echo $field2FontSize;?>px !important;
+	text-align: center;
+}
 #nteam .member
 {
 	width: <?php echo $width;?>px;
 	height: auto;
-	padding:5px;
+	padding: 5px;
 	border-style: solid;
    	border-width: <?php echo $border_width;?>px;
 	border-color: <?php echo $color;?>;
 	overflow: hidden;
-	-webkit-transform:scale(0.8); /*Webkit: Scale down image to 0.8x original size*/
-	-moz-transform:scale(0.8); /*Mozilla scale version*/
-	-o-transform:scale(0.8); /*Opera scale version*/
+	-webkit-transform: scale(0.8); /*Webkit: Scale down image to 0.8x original size*/
+	-moz-transform: scale(0.8); /*Mozilla scale version*/
+	-o-transform: scale(0.8); /*Opera scale version*/
+	-ms-transform: scale(0.8); /*Internet Explorer*/
 	-webkit-transition-duration: 0.5s; /*Webkit: Animation duration*/
 	-moz-transition-duration: 0.5s; /*Mozilla duration version*/
 	-o-transition-duration: 0.5s; /*Opera duration version*/
+	-ms-transition-duration: 0.5s; /*Internet Explorer*/
+	transition-duration: 0.5s;
 	opacity: 0.8; /*initial opacity of images*/
 	margin: 0 -6px; /*margin between images*/
 	float: left;
 }
 #nteam .member:hover
 {	
-	-webkit-transform:scale(1.0); /*Webkit: Scale up image to 1.2x original size*/
-	-moz-transform:scale(1.0); /*Mozilla scale version*/
-	-o-transform:scale(1.0); /*Opera scale version*/
-	box-shadow:0px 0px 20px #ABABAB; /*CSS3 shadow: 30px blurred shadow all around image*/
-	-webkit-box-shadow:0px 0px 20px #ABABAB; /*Safari shadow version*/
-	-moz-box-shadow:0px 0px 20px #ABABAB; /*Mozilla shadow version*/
+	transform: scale(1.0);
+	-webkit-transform: scale(1.0); /*Webkit: Scale up image to 1.2x original size*/
+	-moz-transform: scale(1.0); /*Mozilla scale version*/
+	-o-transform: scale(1.0); /*Opera scale version*/
+	-ms-transform: scale(1); /*Internet Explorer*/
+	box-shadow: 0px 0px 20px #ABABAB; /*CSS3 shadow: 30px blurred shadow all around image*/
+	-webkit-box-shadow: 0px 0px 20px #ABABAB; /*Safari shadow version*/
+	-moz-box-shadow: 0px 0px 20px #ABABAB; /*Mozilla shadow version*/
+	-o-box-shadow: 0px 0px 20px #ABABAB; /*Mozilla shadow version*
+	-ms-box-shadow: 0px 0px 20px #ABABAB; /*Mozilla shadow version*
 	opacity: 1;
 }
 #nteam img
@@ -57,13 +86,5 @@ $height = $_SESSION['height'];
 #nteam .details
 {
 	text-align: center;
-}
-#nteam a
-{
-	text-decoration:none;
-}
-.clearfi
-{
-	clear: both;
 }
 </style>
--- tmpl/default.php
+++ tmpl/default.php
@@ -13,12 +13,21 @@ $_SESSION['color'] = $params->get('border-color');
 $_SESSION['width'] = $width = $params->get('image-width');
 $_SESSION['height'] = $height = $params->get('image-height');
 $_SESSION['border_width'] = $params->get('border-width');
+$_SESSION['titleFontSize'] = $params->get('title-size');
+$_SESSION['field1FontSize'] = $params->get('field1-size');
+$_SESSION['field2FontSize'] = $params->get('field2-size');
 include_once('modules/mod_nteam/css/mod_nteam.php');
-include("resize-class.php");
+include_once("resize-class.php");
 ?>
 <div id="nteam">
 	<div class="container">
 		<?php 
+		$method = $params->get('image-method');
+		$tab = $params->get('link-tab');
+		if($tab == 'new')
+			$target = "_blank";
+		else
+			$target = "_self";
 		for($i=1; $params->get('name_'.$i) != NULL ; $i++)
 		{
 			$name = $params->get('name_'.$i);
@@ -26,43 +35,68 @@ include("resize-class.php");
 			$field2 = $params->get('field2_'.$i);
 			$link = $params->get('link_'.$i);
 			$image = $params->get('image_'.$i);
-			$imageno = $params->get('imageno');
-			$method = $params->get('image-method');
+			if(!file_exists($image))	
+			{
+				$image = 'media/mod_nteam/img/no-image.jpg';
+			}
 			$resizeObj = new resize($image);
 			$resizeObj -> resizeImage( $width, $height, $method);
 			$resizeObj -> saveImage('media/mod_nteam/'.basename($image), 100);
 			$image = 'media/mod_nteam/'.basename($image);
+			$imageno = $params->get('imageno');
 
 		?>
 			<div class="member">
 				<div class="details">
-					<?php if($link != NULL)?>
-						<a href="<?php echo $link;?>" target="_blank">
-					<?php if($image!=NULL)?>
-						<img alt="<?php echo $name;?>" src="<?php echo $image;?>" height="170px"/>
+					<?php 
+					if($link != "")
+					{
+					?>
+					<a href="<?php echo $link;?>" target="<?php echo $target;?>">
+					<img alt="<?php echo $name;?>" src="<?php echo $image;?>" height="170px"/>
 					</a>
-					<?php if($link != NULL)?>
-					<a href="<?php echo $link;?>" target="_blank">
-						<h2><?php echo $name;?></h2>
-					</a>
-					<?php if($field1!= NULL)
-					{?>
-					<h5><?php echo $field1;?></h5>
 					<?php
 					}
-					if($field2!= NULL)
-					{?>
-					<h5><?php echo $field2;?></h5>
-					<?php } ?>
+					else
+					{
+						echo '<img alt="'.$name.'" src="'.$image.'" height="170px"/>';
+					}
+					if($link != "")
+					{
+					?>
+						<a href="<?php echo $link;?>" target="<?php echo $target;?>">
+							<h2 class="title"><?php echo $name;?></h2>
+						</a>
+					<?php
+					}
+					else 
+					{
+						echo '<h2 class="title">'.$name.'</h2>';
+					}
+					?>
+					<?php 
+					if($field1!= "")
+					{
+					?>
+						<h5 class="field1"><?php echo $field1;?></h5>
+					<?php
+					}
+					if($field2!= "")
+					{
+					?>
+						<h5 class="field2"><?php echo $field2;?></h5>
+					<?php 
+					} 
+					?>
 				</div>
 			</div>
 			<?php
-			if($i%$imageno == 0) 
+			if($imageno!=0 && $i%$imageno == 0) 
 			{?>
-			<div class="clearfi"></div>
+			<div style="clear:both;"></div>
 			<?php }
 		}?>
 	</div>
 </div>
-<div class="clearfix"></div>
+<div style="clear:both;"></div>
 	
